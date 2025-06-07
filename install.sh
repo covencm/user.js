92c7467549095f2e3a9892b01b23304b4467fa70
esriee <madhukamj@proton.me>
2025-06-07 19:41:45 +0530

    lil change

--- install.sh
+++ install.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+echo "WARNING: This script will:"
+echo "1. DELETE ALL data in your Firefox default-release profile"
+echo "   This includes bookmarks, passwords, history, extensions, and settings"
+echo "2. Download and install uBlock Origin extension"
+echo ""
+echo "Make sure you have backed up any important Firefox data before proceeding!"
+echo ""
+read -p "Do you want to continue? (y/N): " confirmation
+
+case $confirmation in
+[Yy]*)
+  echo "Proceeding with Firefox profile reset..."
+  ;;
+*)
+  echo "Operation cancelled."
+  exit 0
+  ;;
+esac
 
 pkill firefox
 rm -rf $HOME/.mozilla/firefox/*.default-release/*
