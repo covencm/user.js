1ace785f8dae3f931238c0e224adb3a44f4c0144
covencm <106861998+covencm@users.noreply.github.com>
2026-01-19 20:40:21 +0530

    update ublock 1.68.0

--- install.sh
+++ install.sh
@@ -22,6 +22,6 @@ pkill firefox
 rm -rf $HOME/.mozilla/firefox/*.default-release/*
 cp firefox/* $HOME/.mozilla/firefox/*.default-release
 
-ublock_version="1.64.0"
+ublock_version="1.68.0"
 wget -O uBlock0_$ublock_version.firefox.signed.xpi https://github.com/gorhill/uBlock/releases/download/$ublock_version/uBlock0_$ublock_version.firefox.signed.xpi
 setsid -f firefox uBlock0_$ublock_version.firefox.signed.xpi
